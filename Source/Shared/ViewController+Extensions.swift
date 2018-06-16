1a33298e711c8f8a6f85eb5918be86bc7d49a938
Christoffer Winterkvist <christoffer@winterkvist.com>
2018-06-16 18:39:44 +0200

    Move view controller injection check to shared extension

--- Source/Shared/ViewController+Extensions.swift
+++ Source/Shared/ViewController+Extensions.swift
@@ -5,6 +5,33 @@
 #endif
 
 extension ViewController {
+  func viewControllerWasInjected(_ notification: Notification) -> Bool {
+    if Injection.swizzleViewControllers { return true }
+    if Injection.objectWasInjected(self, notification: notification) { return true }
+    guard let object = Injection.object(from: notification) else {
+      return Injection.swizzleViewControllers
+    }
+
+    var shouldRespondToInjection: Bool = false
+
+    /// Check if parent view controller should be injected.
+    if !childViewControllers.isEmpty {
+      for childViewController in childViewControllers {
+        if object.classForCoder == childViewController.classForCoder {
+          shouldRespondToInjection = true
+          break
+        }
+      }
+    }
+
+    /// Check if object matches self.
+    if !shouldRespondToInjection {
+      shouldRespondToInjection = object.classForCoder == self.classForCoder
+    }
+
+    return shouldRespondToInjection
+  }
+
   public static func _swizzleViewControllers() {
     #if DEBUG
       DispatchQueue.once(token: "com.zenangst.Vaccine.swizzleViewControllers") {
