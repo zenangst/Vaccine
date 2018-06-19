37440d3fe6a7c101a544cc3f7802754025086246
Christoffer Winterkvist <christoffer@winterkvist.com>
2018-06-19 08:22:37 +0200

    Use loadView instead of viewDidLoad for swizzling on view controllers

--- Source/Shared/ViewController+Extensions.swift
+++ Source/Shared/ViewController+Extensions.swift
@@ -35,8 +35,8 @@ extension ViewController {
   public static func _swizzleViewControllers() {
     #if DEBUG
       DispatchQueue.once(token: "com.zenangst.Vaccine.swizzleViewControllers") {
-        let originalSelector = #selector(viewDidLoad)
-        let swizzledSelector = #selector(vaccine_viewDidLoad)
+        let originalSelector = #selector(loadView)
+        let swizzledSelector = #selector(vaccine_loadView)
         Swizzling.swizzle(ViewController.self,
                           originalSelector: originalSelector,
                           swizzledSelector: swizzledSelector)
@@ -44,8 +44,8 @@ extension ViewController {
     #endif
   }
 
-  @objc func vaccine_viewDidLoad() {
-    vaccine_viewDidLoad()
+  @objc func vaccine_loadView() {
+    vaccine_loadView()
     Injection.addViewController(self)
   }
 }
