42e16c2a3ce9bfbb7fb9a0d88f810b1865b35752
Christoffer Winterkvist <christoffer@winterkvist.com>
2018-11-14 14:38:20 +0100

    Fix broken example

--- Example/VaccineDemo/VaccineDemo/Sources/App/AppDelegate.swift
+++ Example/VaccineDemo/VaccineDemo/Sources/App/AppDelegate.swift
@@ -10,7 +10,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
   func application(_ application: UIApplication,
                    didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
-    Injection.load(loadApp).add(observer: self, with: #selector(injected(_:)))
+    Injection.load(then: loadApp).add(observer: self, with: #selector(injected(_:)))
     return true
   }
 
